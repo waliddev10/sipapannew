22a49c83d2f6f9b670cc3e1cd270c95c66ef6e73
walidark <mohwalidas2@gmail.com>
2022-05-19 14:47:55 +0800

    (refactor) Tambah nama bulan di masa pajak modal

--- resources/views/pages/database/masa-pajak/create.blade.php
+++ resources/views/pages/database/masa-pajak/create.blade.php
@@ -4,7 +4,19 @@
 
     <div class="form-group">
         <label class="font-weight-semibold">Bulan</label>
-        <input type="number" name="bulan" class="form-control" />
+        {{-- <input type="number" name="bulan" class="form-control" /> --}}
+        <select name="bulan" class="form-control">
+            @php
+            $bulan = 1;
+            @endphp
+            @while ($bulan <= 12) <option value="{{ $bulan }}">{{ str_pad($bulan, 2, '0', STR_PAD_LEFT) . ' ' .
+                date("F", mktime(0,
+                0, 0, $bulan, 1)) }}</option>
+                @php
+                $bulan++;
+                @endphp
+                @endwhile
+        </select>
     </div>
     <div class="form-group">
         <label class="font-weight-semibold">Tahun</label>
